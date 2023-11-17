ac2492208b8c3e4cbe374cda1affbeae579cc99b
Alina Zaytseva <Alina3aytseva@yandex.ru>
2023-11-17 12:16:21 +1000

    Задание 3: веб-приложение

---
+++ src/main/java/config/WebDriverFactory.java
@@ -0,0 +1,41 @@
+package config;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.concurrent.TimeUnit;
+
+public class WebDriverFactory {
+    //    public static WebDriver get(String browserName) throws RuntimeException {
+//        switch (browserName) {
+//            case "chrome":
+//                return new ChromeDriver();
+//            case "yandex":
+//                System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+//                ChromeOptions options = new ChromeOptions();
+//                options.setBinary("C:/Users/AlinaZaytseva/AppData/Local/Yandex/YandexBrowser/Application/browser.exe");
+//                return new ChromeDriver(options);
+//            default:
+//                throw new RuntimeException("Браузер не выбран");
+//        }
+//    }
+//}
+    public static WebDriver get(String browserName) throws RuntimeException {
+        switch (browserName) {
+            case "chrome":
+                ChromeDriver chromeDriver = new ChromeDriver();
+                chromeDriver.manage().window().maximize();
+                chromeDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+                return chromeDriver;
+            case "yandex":
+                System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+                ChromeOptions options = new ChromeOptions();
+                options.setBinary("C:/Users/AlinaZaytseva/AppData/Local/Yandex/YandexBrowser/Application/browser.exe");
+                ChromeDriver yandexDriver = new ChromeDriver(options);
+                yandexDriver.manage().window().maximize();
+                yandexDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+                return yandexDriver;
+            default:
+                throw new RuntimeException("Браузер не выбран");
+        }
+    }
+}
